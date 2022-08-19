653f497310a058cca61f551d59a2498ee055211e
Thomas Prouvot <prouvot.t@gmail.com>
2022-08-19 17:46:42 +0200

    WIP

--- force-app/main/default/lwc/launchBatch/launchBatch.js
+++ force-app/main/default/lwc/launchBatch/launchBatch.js
@@ -1,15 +1,51 @@
-import { LightningElement, api } from 'lwc';
-import getAccountDocuments from '@salesforce/apex/LaunchBatchController.executeBatchJob';
+import { LightningElement, api, wire, track } from 'lwc';
+import executeBatchJob from '@salesforce/apex/LaunchBatchController.executeBatchJob';
+import getBatchJobStatus from '@salesforce/apex/LaunchBatchController.getBatchJobStatus';
+import { getRecord } from 'lightning/uiRecordApi';
+import { refreshApex } from '@salesforce/apex';
 
+//TODO use lwc encapsulation to pass those values
+import JOB_ID from '@salesforce/schema/MaskSObject__c.LastJobId__c';
+import ERROR_RECORDS from '@salesforce/schema/AsyncApexJob.NumberOfErrors';
+import ITEMS_PROCESSED from '@salesforce/schema/AsyncApexJob.JobItemsProcessed';
+import TOTAL_RECORDS from '@salesforce/schema/AsyncApexJob.TotalJobItems';
+import JOB_STATUS from '@salesforce/schema/AsyncApexJob.Status';
+
+//const FIELDS = [JOB_ID];
 export default class LaunchBatch extends LightningElement {
+	@api recordId;
 	@api className = '';
-	@api batchSize;
+	@api batchSize = 200;
 
-	progress = 50;
+	@track progress = 100;
+	@track isBulk = false;
+	@track processStatus = 'In Progress';
+
 	isProgressing = false;
+	status;
+	total;
+	processed;
+	jobId;
+	errors;
+
+	//@wire(getRecord, { recordId: '$recordId', fields: ['MaskSObject__c.LastJobId__c'] })
+	@wire(getRecord, { recordId: '$recordId', fields: [JOB_ID] })
+	wiredRecord({ error, data }) {
+		if (error) {
+			//TODO handle error
+			console.error(error);
+		} else if (data) {
+			this.jobId = data.fields.LastJobId__c.value;
+
+		}
+	}
 
 	get computedLabel() {
 		return this.isProgressing ? 'Stop Execution' : 'Launch ' + this.className;
+	}
+
+	renderedCallback() {
+		this.runJobStatusReq();
 	}
 
 	toggleProgress() {
@@ -22,21 +58,13 @@ export default class LaunchBatch extends LightningElement {
 				batchName: this.className,
 				scopeSize: this.batchSize
 			}).then(result => {
-				if (this.data) {
-					console.log(this.data);
-				}
+				console.log(JSON.stringify(result));
+				this.jobId = result.Id;
+				this.getJobStatus();
 			})
-				.catch(error => {
-					console.log(error);
+				.catch((error) => {
+					// Handle errors
 				});
-
-			//TODO REMOVE test
-			// start
-			this.isProgressing = true;
-			// eslint-disable-next-line @lwc/lwc/no-async-operation
-			this._interval = setInterval(() => {
-				this.progress = this.progress === 100 ? 0 : this.progress + 1;
-			}, 200);
 		}
 	}
 
@@ -45,5 +73,37 @@ export default class LaunchBatch extends LightningElement {
 		// and the progress is being increased
 		// this code doesn't show in the example
 		clearInterval(this._interval);
+	}
+
+	getJobStatus() {
+		getBatchJobStatus({ jobId: this.jobId })
+			.then(result => {
+				this.status = result.Status;
+				console.log('Status is:' + this.status);
+				this.total = result.TotalJobItems;
+				console.log('Total Records:' + this.total);
+				this.processed = result.JobItemsProcessed;
+				console.log('Records Processed:' + this.processed);
+				this.errRec = result.NumberOfErrors;
+				console.log('error :' + this.errRec);
+			});
+		this.progress = (this.processed / this.total) * 100;
+		console.log('Progress is:' + this.progress);
+		this.processStatus = 'Processing => ' + this.progress + '/' + this.total;
+		if (this.progress === this.total) {
+			clearInterval(this._interval);
+			this.processStatus = 'Completed';
+		}
+	}
+
+	runJobStatusReq() {
+		this._interval = setInterval(() => {
+			this.getJobStatus();
+		}, 5000);
+		if (this.processStatus == 'Completed') {
+			console.log('Refreshing Wire Status');
+			//will call my wire method to refresh the results once batch process completes
+			return refreshApex(this._wiredResult);
+		}
 	}
 }
